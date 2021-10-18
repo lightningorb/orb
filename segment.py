7409ce3fb0c731823b547d6b11d8e82312b0b910
orb <admin@lnorb.com>
2021-10-18 15:36:19 +0800

    forgot to check in

--- segment.py
+++ segment.py
@@ -1,25 +1,30 @@
+from kivy.uix.widget import Widget
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
 from kivy.graphics.vertex_instructions import Ellipse
 from lerp import lerp_2d
 from math import ceil
+from colors import *
 
 
-class Segment(Line):
-    def __init__(self, amount=0, *args, **kwargs):
+class Segment(Widget):
+    def __init__(self, points, width, cap, amount=0, color=None, *args, **kwargs):
         super(Segment, self).__init__(*args, **kwargs)
         self.amount = amount
         self.d = 3
         self.r = self.d / 2
-        Color([1, 0, 0, 1])
-        self.e = [
-            Ellipse(pos=[0, 0], size=[self.d, self.d])
-            for _ in range(ceil(self.amount / 1e6))
-        ]
+        with self.canvas:
+            self.color = Color(*color)
+            self.line = Line(points=points, width=width, cap=cap)
+            Color(*BRIGHT_GREEN)
+            self.e = [
+                Ellipse(pos=[0, 0], size=[self.d, self.d])
+                for _ in range(ceil(self.amount / 1e6))
+            ]
 
     def update_rect(self, amount=0):
-        a = lerp_2d(self.points[:2], self.points[2:], 0.02)
-        b = lerp_2d(self.points[:2], self.points[2:], 0.98)
+        a = lerp_2d(self.line.points[:2], self.line.points[2:], 0.02)
+        b = lerp_2d(self.line.points[:2], self.line.points[2:], 0.98)
         n = ceil(amount / 1e6)
         # if n > len(self.e):
         #     self.e.append(Ellipse(pos=[0, 0], size=[self.d, self.d]))
