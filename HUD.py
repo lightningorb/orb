9451771fccc6661968a91b19bbe993d1f430b784
orb <admin@lnorb.com>
2021-10-21 12:33:57 +0800

    added mempool and price chart to HUD

--- HUD.py
+++ HUD.py
@@ -1,11 +1,19 @@
-import data_manager
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import RoundedRectangle, Line
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from decorators import guarded
+import data_manager
+import threading
+import requests
 
+
+class Bordered(Widget):
+    pass
 
 class BorderedLabel(Label):
     pass
@@ -66,3 +74,52 @@ class HUD3(BorderedLabel):
         dpi_info = Metrics.dpi
         pixel_density_info = Metrics.density
         self.hud = f"DPI: {dpi_info}, Pixel Density: {pixel_density_info}"
+
+class HUD4(FloatLayout):
+    def __init__(self, *args, **kwargs):
+        FloatLayout.__init__(self, *args, **kwargs)
+        with self.canvas.before:
+            Color(1,1,1,1)
+            self.line = Line(points=[0,0,500,500], width=1)
+        self.bind(pos=self.update_rect, size=self.update_rect)
+        Clock.schedule_interval(self.update_price, 60)
+        Clock.schedule_once(self.update_price, 1)
+
+    def update_rect(self, *args):
+        def func():
+            # this probably shouldn't be in update_rect
+            d = requests.get("https://api.coindesk.com/v1/bpi/historical/close.json").json()['bpi']
+            min_price, max_price, g = min(d.values()), max(d.values()), []
+            for i, key in enumerate(d):
+                g.append(i/len(d)*self.size[0])
+                g.append(self.pos[1]+((d[key]-min_price)/(max_price-min_price))*self.size[1])
+            self.line.points = g
+            rate = str(int(requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()['bpi']['USD']['rate_float']))
+            self.ids.rate.text = f'${rate}'
+        threading.Thread(target=func).start()
+
+    def update_price(self, *args):
+        def func():
+            rate = int(requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()['bpi']['USD']['rate_float'])
+            self.ids.rate.text = f'${rate}'
+        threading.Thread(target=func).start()
+
+
+class HUD5(BorderedLabel):
+    hud = ObjectProperty("")
+
+    def __init__(self, *args, **kwargs):
+        BorderedLabel.__init__(self, *args, **kwargs)
+        Clock.schedule_interval(self.get_fees, 60)
+        Clock.schedule_once(self.get_fees, 1)
+
+    @guarded
+    def get_fees(self, *args):
+        def func():
+            fees = requests.get("https://mempool.space/api/v1/fees/recommended").json()
+            text = f"""\
+Low priority: {fees["hourFee"]} sat/vB
+Medium priority: {fees["halfHourFee"]} sat/vB
+High priority: {fees["fastestFee"]} sat/vB"""
+            self.hud = text
+        threading.Thread(target=func).start()
