b6b103754c5fe8f5024331c6de6e40051f8683ad
orb <admin@lnorb.com>
2021-10-25 12:42:56 +0800

    settings labels don't update correctly when settings are changed [issue #49]

--- monkey_patch.py
+++ monkey_patch.py
@@ -6,8 +6,10 @@ def patch_settings():
     def add_widget(self, *largs):
         largs = [*largs]
         if largs and type(largs[0]) is Label:
-            value = self.panel.get_value(self.section, self.key)[:20]
-            largs[0] = Label(text=value)
+            print(self.section, self.key)
+            if self.key in ['tls_certificate', 'macaroon_admin']:
+                value = self.panel.get_value(self.section, self.key)[:20]
+                largs[0] = Label(text=value)
         if self.content is None:
             return super(SettingItem, self).add_widget(*largs)
         return self.content.add_widget(*largs)
