8bdbf45fa53cae62a7383ea033ed5906d6d865c7
orb <admin@lnorb.com>
2021-11-01 19:35:21 +0800

    prevent nodes from catching menu clicks

--- node.py
+++ node.py
@@ -8,12 +8,13 @@ import data_manager
 from threading import Thread
 from utils import prefs_col
 
+
 class Node(Button):
     attribute_editor = ObjectProperty(None)
     col = ListProperty([0, 0, 0, 0])
     channel = ObjectProperty(None)
-    touch_start = ListProperty([0,0])
-    touch_end = ListProperty([0,0])
+    touch_start = ListProperty([0, 0])
+    touch_end = ListProperty([0, 0])
 
     def __init__(self, *args, **kwargs):
         super(Node, self).__init__(*args, **kwargs)
@@ -41,6 +42,14 @@ class Node(Button):
 
     def on_press(self):
         self.col = prefs_col('display.node_selected_background_color')
+
+    def on_touch_down(self, touch):
+        import data_manager
+
+        if self.collide_point(*touch.pos):
+            if data_manager.menu_visible:
+                return False
+        return super(Node, self).on_touch_down(touch)
 
     # def on_touch_down(self, touch):
     #     if self.collide_point(*touch.pos):
