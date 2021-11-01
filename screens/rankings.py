2731b9f3d1c57f44b26f0586c922aafb8f75fa44
orb <admin@lnorb.com>
2021-11-01 16:10:22 +0800

    Orb Crashes when in Lightning/Rankings and the Down arrow next to Alias is selected [issue #65]

--- screens/rankings.py
+++ screens/rankings.py
@@ -1,3 +1,4 @@
+from kivy.uix.label import Label
 from kivy.metrics import dp
 from popup_drop_shadow import PopupDropShadow
 from kivy.uix.screenmanager import Screen
@@ -11,22 +12,36 @@ class Rankings(PopupDropShadow):
         super(Rankings, self).__init__(*args)
         from store.node_rank import count_successes_failures
 
-        self.data_tables = MDDataTable(
-            use_pagination=True,
-            rows_num=25,
-            check=False,
-            column_data=[
-                ("Alias", dp(60)),
-                ("Successes", dp(30), self.sort_on_signal),
-                ("Failures", dp(30)),
-            ],
-            row_data=[x for x in count_successes_failures()],
-            sorted_on="Successes",
-            sorted_order="ASC",
-            elevation=2,
-        )
-        self.add_widget(self.data_tables)
+        row_data = count_successes_failures()
 
-    # [Indexes, Sorted_Row_Data]
+        if row_data:
+            self.data_tables = MDDataTable(
+                use_pagination=True,
+                rows_num=25,
+                check=False,
+                column_data=[
+                    ("Alias", dp(60)),
+                    ("Successes", dp(30), self.sort_on_signal),
+                    ("Failures", dp(30)),
+                ],
+                row_data=row_data,
+                sorted_on="Successes",
+                sorted_order="ASC",
+                elevation=2,
+            )
+            self.add_widget(self.data_tables)
+        else:
+            self.add_widget(
+                Label(
+                    text=(
+                        'This feature ranks nodes by how predictable\n'
+                        'they are at routing payments.\n'
+                        'No path-finding data available.\n'
+                        'Make payments, or circular rebalances.'
+                    )
+                )
+            )
+
+    @guarded
     def sort_on_signal(self, data):
         return zip(*sorted(enumerate(data), key=lambda l: l[1][1]))
