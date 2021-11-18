999d0f9116c9fa9333bcb7cb7271d47e7c651263
orb <admin@lnorb.com>
2021-11-19 07:18:09 +0800

    Can now set a different display currency (forgot to check in).

---
+++ orb/misc/forex.py
@@ -0,0 +1,13 @@
+from forex_python.bitcoin import BtcConverter
+from orb.misc.utils import pref
+from currency_symbols import CurrencySymbols
+
+b = BtcConverter()
+
+
+def forex(sats):
+    currency = pref('display.currency')
+    if currency == 'SAT':
+        return f'S{sats}'
+    symbol = CurrencySymbols.get_symbol(currency)
+    return f'{symbol}{round(b.get_latest_price(currency) * sats / 1e8, 2):,}'
