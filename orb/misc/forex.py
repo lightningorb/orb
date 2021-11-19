981a53c930a5cfeb850319efc09e6e653fdf01ca
orb <admin@lnorb.com>
2021-11-19 14:51:03 +0800

    sat denominations should also have comma on 3 number groups

--- orb/misc/forex.py
+++ orb/misc/forex.py
@@ -8,6 +8,6 @@ b = BtcConverter()
 def forex(sats):
     currency = pref('display.currency')
     if currency == 'SAT':
-        return f'S{sats}'
+        return f'S{sats:,}'
     symbol = CurrencySymbols.get_symbol(currency)
     return f'{symbol}{round(b.get_latest_price(currency) * sats / 1e8, 2):,}'
