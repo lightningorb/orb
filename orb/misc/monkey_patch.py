c5d7db30a91e9081e855a0a06e98731faa4a4678
orb <admin@lnorb.com>
2021-11-21 19:50:39 +0800

    Orb crashes when in Rankings and the alias box is selected. [issue #80]

--- orb/misc/monkey_patch.py
+++ orb/misc/monkey_patch.py
@@ -1,3 +1,66 @@
+def patch_datatables():
+    from kivymd.uix.datatables import TableData
+
+    def select_all(self, state):
+        """Sets the checkboxes of all rows to the active/inactive position."""
+
+        for i in range(0, len(self.recycle_data), self.total_col_headings):
+            cell_row_obj = cell_row_obj = self.view_adapter.get_visible_view(i)
+            self.cell_row_obj_dict[i] = cell_row_obj
+            self.on_mouse_select(cell_row_obj)
+            if cell_row_obj:
+                cell_row_obj.ids.check.state = state
+
+        if state == "down":
+            # select all checks on all pages
+            rows_num = self.rows_num
+            columns = self.total_col_headings
+            full_pages = len(self.row_data) // self.rows_num
+            left_over_rows = len(self.row_data) % self.rows_num
+
+            new_checks = {}
+            for page in range(full_pages):
+                new_checks[page] = list(range(0, rows_num * columns, columns))
+
+            if left_over_rows:
+                new_checks[full_pages] = list(
+                    range(0, left_over_rows * columns, columns)
+                )
+
+            self.current_selection_check = new_checks
+            return
+
+        # resets all checks on all pages
+        self.current_selection_check = {}
+
+    def on_mouse_select(self, instance):
+        """Called on the ``on_enter`` event of the :class:`~CellRow` class."""
+
+        if not self.pagination_menu_open:
+            if instance and self.ids.row_controller.selected_row != instance.index:
+                self.ids.row_controller.selected_row = instance.index
+                self.ids.row_controller.select_current(self)
+
+    TableData.on_mouse_select = on_mouse_select
+
+    def check_all(self, state):
+        """Checks if checkboxes of all rows are in the same state"""
+
+        tmp = []
+        for i in range(0, len(self.recycle_data), self.total_col_headings):
+            if self.cell_row_obj_dict.get(i, None):
+                cell_row_obj = self.cell_row_obj_dict[i]
+            else:
+                cell_row_obj = self.view_adapter.get_visible_view(i)
+                if cell_row_obj:
+                    self.cell_row_obj_dict[i] = cell_row_obj
+            if cell_row_obj:
+                tmp.append(cell_row_obj.ids.check.state == state)
+        return all(tmp)
+
+    TableData.select_all = select_all
+
+
 def patch_settings():
 
     from kivy.uix.settings import SettingItem
@@ -6,7 +69,7 @@ def patch_settings():
     def add_widget(self, *largs):
         largs = [*largs]
         if largs and type(largs[0]) is Label:
-            if self.key in ['tls_certificate', 'macaroon_admin']:
+            if self.key in ["tls_certificate", "macaroon_admin"]:
                 value = self.panel.get_value(self.section, self.key)[:20]
                 largs[0] = Label(text=value)
         if self.content is None:
@@ -28,6 +91,7 @@ def patch_store():
     JsonStore.orig_get = JsonStore.get
     JsonStore.get = get
 
+
 def patch_kv_settings():
     from kivy.uix.settings import Settings
 
@@ -35,11 +99,16 @@ def patch_kv_settings():
         from kivy import kivy_data_dir
         from kivy.config import Config
         from os.path import join
-        self.add_json_panel('App Settings', Config, join(kivy_data_dir, 'settings_kivy.json'))
+
+        self.add_json_panel(
+            "App Settings", Config, join(kivy_data_dir, "settings_kivy.json")
+        )
 
     Settings.add_kivy_panel = add_kivy_panel
+
 
 def do_monkey_patching():
     patch_store()
     patch_settings()
     patch_kv_settings()
+    patch_datatables()
