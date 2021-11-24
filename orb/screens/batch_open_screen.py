c05dd9f70f9382179a78fa1e79dc7d19354dfe38
orb <admin@lnorb.com>
2021-11-25 05:28:34 +0800

    batch open should use fastest fee + 1, so opens don't get stuck

--- orb/screens/batch_open_screen.py
+++ orb/screens/batch_open_screen.py
@@ -13,7 +13,7 @@ from orb.misc import mempool
 
 
 class Tab(MDFloatLayout, MDTabsBase):
-    '''Class implementing content for a tab.'''
+    """Class implementing content for a tab."""
 
 
 class BatchOpenScreen(PopupDropShadow):
@@ -21,7 +21,7 @@ class BatchOpenScreen(PopupDropShadow):
     def open(self, *args):
         from data_manager import data_man
 
-        self.ids.pubkeys.text = data_man.store.get('batch_open', {}).get('text', '')
+        self.ids.pubkeys.text = data_man.store.get("batch_open", {}).get("text", "")
         super(BatchOpenScreen, self).open(*args)
 
     @guarded
@@ -29,9 +29,9 @@ class BatchOpenScreen(PopupDropShadow):
         from data_manager import data_man
 
         pks, amounts = [], []
-        for line in text.split('\n'):
-            if ',' in line:
-                pk, a = [x.strip() for x in line.split(',')]
+        for line in text.split("\n"):
+            if "," in line:
+                pk, a = [x.strip() for x in line.split(",")]
                 pks.append(pk)
                 amounts.append(a)
             else:
@@ -39,26 +39,26 @@ class BatchOpenScreen(PopupDropShadow):
                 if pk:
                     pks.append(pk)
         amounts = [int(int(amount) / len(pks)) for _ in range(len(pks))]
-        self.ids.pubkeys.text = '\n'.join([f'{p},{a}' for p, a in zip(pks, amounts)])
+        self.ids.pubkeys.text = "\n".join([f"{p},{a}" for p, a in zip(pks, amounts)])
 
     def ingest(self, text):
         from data_manager import data_man
 
-        data_man.store.put('batch_open', text=text)
+        data_man.store.put("batch_open", text=text)
 
     @guarded
     def get_pks_amounts(self):
         pks, amounts = [], []
-        for line in self.ids.pubkeys.text.split('\n'):
-            if ',' in line:
-                pk, a = [x.strip() for x in line.split(',')]
+        for line in self.ids.pubkeys.text.split("\n"):
+            if "," in line:
+                pk, a = [x.strip() for x in line.split(",")]
                 pks.append(pk)
                 amounts.append(a)
         return pks, amounts
 
     @guarded
     def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
-        if tab_text == 'confirm':
+        if tab_text == "confirm":
             from data_manager import data_man
 
             pks, amounts = self.get_pks_amounts()
@@ -69,7 +69,7 @@ class BatchOpenScreen(PopupDropShadow):
                     use_pagination=False,
                     check=False,
                     column_data=[("Alias", dp(60)), ("Amount", dp(30))],
-                    row_data=[(al, f'{int(am):,}') for al, am in zip(aliases, amounts)],
+                    row_data=[(al, f"{int(am):,}") for al, am in zip(aliases, amounts)],
                     elevation=2,
                 )
             )
@@ -83,7 +83,7 @@ class BatchOpenScreen(PopupDropShadow):
             response = data_man.lnd.batch_open(
                 pubkeys=pks,
                 amounts=amounts,
-                sat_per_vbyte=mempool.get_fees('halfHourFee'),
+                sat_per_vbyte=mempool.get_fees("fastestFee") + 1,
             )
             self.ids.open_status.text = str(response)
             console_output(str(response))
@@ -96,23 +96,23 @@ class BatchOpenScreen(PopupDropShadow):
         pks, amounts = self.get_pks_amounts()
         from data_manager import data_man
 
-        self.ids.connect.text = ''
+        self.ids.connect.text = ""
 
         @mainthread
         def print(x):
-            self.ids.connect.text += str(x) + '\n'
+            self.ids.connect.text += str(x) + "\n"
 
         def func(*args):
             for pk, amount in zip(pks, amounts):
                 info = data_man.lnd.get_node_info(pk)
-                print('-' * 50)
+                print("-" * 50)
                 print(data_man.lnd.get_node_alias(pk))
-                print('-' * 50)
+                print("-" * 50)
                 for address in info.node.addresses:
-                    print(f'Connecting to: {pk}@{address.addr}')
+                    print(f"Connecting to: {pk}@{address.addr}")
                     try:
-                        data_man.lnd.connect(f'{pk}@{address.addr}')
-                        print('Success.')
+                        data_man.lnd.connect(f"{pk}@{address.addr}")
+                        print("Success.")
                     except Exception as e:
                         print(e.args[0].details)
 
