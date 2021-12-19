3bc7de61ac19b247243e7ee6deb951774cf744cd
orb <admin@lnorb.com>
2021-12-19 08:51:39 +0800

    update max HTLC to half cap

--- user/scripts/update_max_htlc.py
+++ user/scripts/update_max_htlc.py
@@ -2,7 +2,7 @@
 # @Author: lnorb.com
 # @Date:   2021-12-15 07:27:21
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2021-12-15 07:27:24
+# @Last Modified time: 2021-12-19 08:37:05
 
 import data_manager
 from time import sleep
@@ -18,23 +18,22 @@ class UpdateMaxHTLC(Thread):
         Clock.schedule_interval(lambda _: Thread(target=self.main).start(), 30 * 60)
 
     def main(self, *_):
-        print(f"Checking policies")
         for i, c in enumerate(lnd.get_channels()):
             policy = lnd.get_policy_to(c.chan_id)
-            round = lambda x: int(int(x / 1000) * 10_00)
-            max_htlc = round(int((policy.max_htlc_msat) / 1000))
-            local = round(int(c.local_balance))
-            needs_update = max_htlc != local
+            round = lambda x: int(int(x / 1_000) * 1_000)
+            max_htlc = round(int(policy.max_htlc_msat / 1_000))
+            half_cap = round(int(c.capacity / 2))
+            needs_update = max_htlc != half_cap
             if needs_update:
                 print(
-                    f"Updating policy for: {c.chan_id}, max_htlc: {max_htlc}, local: {local}"
+                    f"Updating policy for: {c.chan_id}, max_htlc: {max_htlc}, half_cap: {half_cap}"
                 )
                 lnd.update_channel_policy(
                     channel=c,
                     time_lock_delta=int(policy.time_lock_delta),
                     fee_rate=int(policy.fee_rate_milli_msat) / 1e6,
                     base_fee_msat=int(policy.fee_base_msat),
-                    max_htlc_msat=int((c.local_balance * 1000)),
+                    max_htlc_msat=int((half_cap * 1_000)),
                 )
         print(f"Max HTLC updated")
 
--- user/scripts/user_setup.py
+++ user/scripts/user_setup.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: lnorb.com
+# @Date:   2021-12-10 07:33:21
+# @Last Modified by:   lnorb.com
+# @Last Modified time: 2021-12-19 08:35:16
+
 import update_max_htlc
 
 update_max_htlc.main()
