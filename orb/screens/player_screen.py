03d600cd6ee15d46d61a1ee56f6060aa57bc8e38
orb <admin@lnorb.com>
2022-01-03 08:20:18 +0800

    different vid

--- orb/screens/player_screen.py
+++ orb/screens/player_screen.py
@@ -2,7 +2,7 @@
 # @Author: lnorb.com
 # @Date:   2021-12-15 07:15:28
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2021-12-30 17:58:01
+# @Last Modified time: 2022-01-02 07:07:15
 
 import os
 from kivy.uix.screenmanager import Screen
@@ -15,9 +15,7 @@ class PlayerScreen(Screen):
     def on_enter(self):
         if not self.built:
             player = VideoPlayer(
-                source=os.path.expanduser(
-                    "~/Movies/Monosnap/screencast 2021-12-29 17-28-56.mp4"
-                )
+                source=os.path.expanduser("~/Desktop/vids/batch_open.mp4")
             )
             self.ids.bl.add_widget(player)
             self.built = True
