1549cea2fa6c5e2a7971d36972f840eda74d74b2
orb <admin@lnorb.com>
2022-01-03 08:19:56 +0800

    mail screen improvements

--- orb/screens/mail_screen.py
+++ orb/screens/mail_screen.py
@@ -2,39 +2,45 @@
 # @Author: lnorb.com
 # @Date:   2021-12-15 07:15:28
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2021-12-31 06:19:39
+# @Last Modified time: 2022-01-03 07:43:18
 
 import time
 
 from kivy.clock import mainthread
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen
+from kivy.uix.textinput import TextInput
 
 from orb.lnd import Lnd
 
 
+class FocusTextInput(TextInput):
+    def on_touch_down(self, touch):
+        import data_manager
+
+        if self.collide_point(*touch.pos):
+            if data_manager.menu_visible:
+                return False
+        return super(FocusTextInput, self).on_touch_down(touch)
+
+
 class MailScreen(Screen):
     def on_enter(self):
-        self.ids.inbox.clear_widgets()
+        text = ""
         for invoice in Lnd().list_invoices().invoices:
-            if not invoice.settled:
-                continue
-            print(invoice)
-            htlc_records = []
-            for htlc in invoice.htlcs:
-                custom_records = htlc.custom_records
-                if custom_records:
-                    htlc_records.append(custom_records)
-
-            if htlc_records:
-                self.ids.inbox.add_widget(Label(text=time.ctime(invoice.settle_date)))
-                for custom_records in htlc_records:
-                    for k, v in custom_records.items():
-                        if k == 34349334:
+            if invoice.settled:
+                htlc_records = [
+                    htlc.custom_records for htlc in invoice.htlcs if htlc.custom_records
+                ]
+                if htlc_records:
+                    text += f"{time.ctime(invoice.settle_date)}:\n\n"
+                    for r in htlc_records:
+                        if r.get(34349334, ""):
+                            msg = str(r.get(34349334, "")).strip("b'")
                             try:
-                                msg = v.decode()
+                                msg = r.get(34349334, "").decode()
                             except UnicodeDecodeError:
-                                msg = str(v).strip("b'")
-                            if not msg:
-                                msg = "<empty message>"
-                            self.ids.inbox.add_widget(Label(text=msg))
+                                pass
+                            if msg:
+                                text += f"{msg}\n\n"
+        self.ids.input.text = text
