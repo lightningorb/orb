0b7fbe17c5eb2c657695266039c3ba32caac4967
orb <admin@lnorb.com>
2022-01-08 07:32:29 +0800

    improved alias caching

--- orb/store/db_cache.py
+++ orb/store/db_cache.py
@@ -1,17 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Author: lnorb.com
+# @Date:   2022-01-06 17:51:07
+# @Last Modified by:   lnorb.com
+# @Last Modified time: 2022-01-08 07:29:35
+
 import functools
+from threading import Lock
+from orb.store import model
+from functools import lru_cache
+
+lock = Lock()
+cache = {}
 
 
 def aliases_cache(func):
-    from orb.store import model
+    """
+    Cache decorator for getting node aliases. This leads to
+    a significant speedup as there can be many peers.
+    """
 
     @functools.wraps(func)
     def wrapper_decorator(*args, **kwargs):
+        """
+        functools.wraps things. Seriously what does this do again?
+        """
         pk = args[1]
-        alias = model.Alias().select().where(model.Alias.pk == pk)
-        if alias:
-            return alias.get().alias
-        alias = func(*args, **kwargs)
-        model.Alias(pk=pk, alias=alias).save()
-        return alias
+
+        if pk in cache:
+            return cache[pk]
+        with lock:
+            alias = model.Alias().select().where(model.Alias.pk == pk)
+            if alias:
+                cache[pk] = alias.get().alias
+                return cache[pk]
+            alias = func(*args, **kwargs)
+            model.Alias(pk=pk, alias=alias).save()
+            cache[pk] = alias
+        return cache[pk]
 
     return wrapper_decorator
