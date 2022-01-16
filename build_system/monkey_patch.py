0bb6cd2513fe994ac6d5866df1a4d1d58bb41f44
orb <admin@lnorb.com>
2022-01-16 09:42:23 +0800

    [dev] fabric monkeypatch, and combo command for ios deploy

---
+++ build_system/monkey_patch.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Author: lnorb.com
+# @Date:   2022-01-16 07:58:17
+# @Last Modified by:   lnorb.com
+# @Last Modified time: 2022-01-16 07:58:25
+
+from unittest.mock import patch
+from inspect import getfullargspec, ArgSpec
+
+import invoke
+
+
+def fix_annotations():
+    """
+    Pyinvoke doesnt accept annotations by default, this fix that
+    Based on: https://github.com/pyinvoke/invoke/pull/606
+    """
+
+    def patched_inspect_getargspec(func):
+        spec = getfullargspec(func)
+        return ArgSpec(*spec[0:4])
+
+    org_task_argspec = invoke.tasks.Task.argspec
+
+    def patched_task_argspec(*args, **kwargs):
+        with patch(target="inspect.getargspec", new=patched_inspect_getargspec):
+            return org_task_argspec(*args, **kwargs)
+
+    invoke.tasks.Task.argspec = patched_task_argspec
--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,15 @@
 # @Author: lnorb.com
 # @Date:   2022-01-13 06:45:34
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-01-14 22:35:12
+# @Last Modified time: 2022-01-16 08:15:21
 
 import re
 import os
 from invoke import task, Context, Collection
+from build_system.monkey_patch import fix_annotations
+
+fix_annotations()
+
 from build_system import third_party
 from build_system import versioning
 from build_system import ios
@@ -17,6 +21,22 @@ from build_system import test
 from build_system import release_notes
 
 
+@task
+def deploy_ios(c, bump: bool = False):
+    if bump:
+        versioning.bump_patch(c)
+    release_notes.create(c)
+    ios.update(c)
+
+
 namespace = Collection(
-    third_party, versioning, ios, submodules, documentation, test, release_notes, tags
+    deploy_ios,
+    third_party,
+    versioning,
+    ios,
+    submodules,
+    documentation,
+    test,
+    release_notes,
+    tags,
 )
