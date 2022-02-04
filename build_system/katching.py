cf3b1b1abde6d72fa309d5789f3332e23a35fd82
orb <admin@lnorb.com>
2022-02-05 07:26:34 +0800

    lnd invoice service

--- build_system/katching.py
+++ build_system/katching.py
@@ -2,13 +2,14 @@
 # @Author: lnorb.com
 # @Date:   2022-02-05 05:46:07
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-02-05 07:00:32
+# @Last Modified time: 2022-02-05 07:24:01
 
 """
 Everything related to recieving and processing licence payments on
 the LND node.
 
 ./build.py katching.deploy-katching
+./build.py katching.deploy-lnd-invoice-service
 
 """
 
@@ -18,24 +19,29 @@ from invoke import task
 
 
 @task
-def deploy_katching_service(c):
-    """
-    The Katching Service scans for SETTLED invoices on the
-    LND node, and dispatches them as jobs on RabbitMQ.
-    """
-    with c.cd("orb"):
-        c.run("git stash")
-        c.run("git pull")
-        c.put("build_system/katching.conf", "/tmp/")
-    c.sudo("mv /tmp/katching.conf /etc/supervisor/conf.d/")
-    c.sudo("supervisorctl update")
-
-
-@task
-def deploy_lnd_invoice_service(c):
+def deploy_lnd_invoice_service(c, env=os.environ):
     """
     The LND Invoice Service runs on the node, and generates
     invoices by handling messages dispatched via RabbitMQ.
+    """
+    with Connection(
+        env["LND_NODE_IP"],
+        connect_kwargs={"key_filename": env["LND_NODE_SSH_CERT"]},
+        user="ubuntu",
+    ) as con:
+        with con.cd("orb"):
+            con.run("git stash")
+            con.run("git pull")
+            con.put("build_system/lnd_invoice_service.conf", "/tmp/")
+        con.sudo("mv /tmp/lnd_invoice_service.conf /etc/supervisor/conf.d/")
+        con.sudo("supervisorctl update")
+
+
+@task
+def deploy_katching_service(c, env=os.environ):
+    """
+    The Katching Service scans for SETTLED invoices on the
+    LND node, and dispatches them as jobs on RabbitMQ.
     """
     with c.cd("orb"):
         c.run("git stash")
