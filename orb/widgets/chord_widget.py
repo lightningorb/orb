d31ca0fbe34e6f456681fcf44f7e57825c6665fa
orb <admin@lnorb.com>
2022-02-10 07:14:53 +0800

    [dev] some refactoring in chord widget

--- orb/widgets/chord_widget.py
+++ orb/widgets/chord_widget.py
@@ -2,13 +2,14 @@
 # @Author: lnorb.com
 # @Date:   2021-12-15 07:15:28
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2021-12-29 18:31:49
+# @Last Modified time: 2022-02-10 06:42:53
 
 import math
 from collections import defaultdict
 from functools import cmp_to_key
 from threading import Thread
 from random import shuffle, randrange
+from functools import lru_cache
 
 from colour import Color as Colour
 
@@ -18,6 +19,7 @@ from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
 from kivy.graphics import Mesh
 from kivy.graphics.tesselator import Tesselator
+from kivy.animation import Animation
 from kivy.app import App
 from kivy.clock import mainthread
 
@@ -36,20 +38,20 @@ class ChordWidget(Widget):
     def __init__(self, channels, *args, **kwargs):
         super(ChordWidget, self).__init__(*args, **kwargs)
         self.channels = channels
-        Clock.schedule_once(lambda _: Thread(target=self.update_rect).start(), 1)
-        channels.bind(channels=self.update_rect)
-        data_manager.data_man.bind(show_chords=self.update_rect)
+        Clock.schedule_once(lambda _: Thread(target=self.update).start(), 1)
+        channels.bind(channels=self.update)
+        data_manager.data_man.bind(show_chords=self.update)
         data_manager.data_man.bind(show_chord=self.show_chord)
-        data_manager.data_man.bind(chords_direction=self.update_rect)
+        data_manager.data_man.bind(chords_direction=self.update)
 
     def show_chord(self, inst, chord):
         chord %= len(self.channels)
         show = {str(c.chan_id): i == chord for i, c in enumerate(self.channels)}
         data_manager.data_man.store.put("show_to_chords", **show)
-        self.update_rect()
+        self.update()
 
     @mainthread
-    def update_rect(self, *args, **kwargs):
+    def update(self, *args, **kwargs):
         self.canvas.clear()
 
         if not data_manager.data_man.show_chords:
@@ -98,10 +100,6 @@ class ChordWidget(Widget):
     def draw_channel_chords(
         self, out_chan, col, chan_pos, matrix, offset, sec_w, sec_w2, chan_cols
     ):
-        def offset_to_pos(offset):
-            x = math.sin(offset / 360 * 3.14378 * 2) * self.radius
-            y = math.cos(offset / 360 * 3.14378 * 2) * self.radius
-            return (x, y)
 
         in_chans = [x[1] for x in matrix if x[0] == out_chan and x[1] in chan_pos]
 
@@ -112,39 +110,9 @@ class ChordWidget(Widget):
 
         in_chans.sort(key=cmp_to_key(sort_by_pos))
 
-        def get_line_points(out_chan_deg, in_chan_deg, inverted=False):
-            x, y = offset_to_pos(out_chan_deg)
-            x1, y1 = offset_to_pos(in_chan_deg)
-            a, b = Vector(x, y), Vector(x1, y1)
-            dot = a.normalized().dot(b.normalized())
-            b_mid_x, b_mid_y = lerp_vec(Vector(0, 0), a.mid(b), max(0, dot))
-            import bezier
-
-            curve1 = bezier.Curve(
-                [
-                    [x, b_mid_x, x1],
-                    [y, b_mid_y, y1],
-                ],
-                degree=2,
-            )
-            points = []
-            step = 100
-            for f in range(step):
-                e = curve1.evaluate(f / step)
-                points.extend([float(e[int(inverted)]), float(e[int(not inverted)])])
-            return points if not inverted else points[::-1]
-
-        use_total_liq = False
-        if use_total_liq:
-            chan_ids = [x.chan_id for x in self.channels]
-            total_liq = sum(
-                matrix[(x, y)]
-                for x in chan_ids
-                for y in chan_ids
-                if x != y and (x, y) in matrix
-            )
-        else:
-            total_liq = sum(matrix[(out_chan, in_chan)] for in_chan in in_chans)
+        total_liq = self.get_total_liq(
+            matrix=matrix, out_chan=out_chan, in_chans=in_chans
+        )
 
         local_offset = offset - sec_w2
         dist = (offset + sec_w2) - local_offset
@@ -152,11 +120,11 @@ class ChordWidget(Widget):
             liq = matrix[(out_chan, in_chan)] / total_liq
             out_chan_deg, in_chan_deg = chan_pos[out_chan], chan_pos[in_chan]
             tess = Tesselator()
-            points1 = get_line_points(
+            points1 = self.get_line_points(
                 out_chan_deg + local_offset, in_chan_deg + (liq * sec_w / 2.5)
             )
             local_offset += liq * dist
-            points2 = get_line_points(
+            points2 = self.get_line_points(
                 out_chan_deg + local_offset,
                 in_chan_deg - (liq * sec_w / 2.5),
                 inverted=True,
@@ -164,10 +132,68 @@ class ChordWidget(Widget):
             tess.add_contour(points1 + points2)
             if tess.tesselate():
                 c = chan_cols[in_chan]
-                Color(rgba=(c[0], c[1], c[2], 0.5))
+                col = Color(rgba=(c[0], c[1], c[2], 0), group=str(out_chan))
                 for vertices, indices in tess.meshes:
-                    Mesh(vertices=vertices, indices=indices, mode="triangle_fan")
+                    Mesh(
+                        vertices=vertices,
+                        indices=indices,
+                        mode="triangle_fan",
+                    )
+                Animation(
+                    rgba=col.rgb + [0.5],
+                    duration=0.2,
+                ).start(col)
 
+    def get_total_liq(self, matrix, out_chan, in_chans, total_for_node=False):
+        if total_for_node:
+            chan_ids = [x.chan_id for x in self.channels]
+            return sum(
+                matrix[(x, y)]
+                for x in chan_ids
+                for y in chan_ids
+                if x != y and (x, y) in matrix
+            )
+        else:
+            return sum(matrix[(out_chan, in_chan)] for in_chan in in_chans)
+
+    @lru_cache(maxsize=None)
+    def offset_to_pos(self, offset):
+        """
+        Channels positions come in degrees. compute an x, y coordinate
+        in local space.
+        """
+        x = math.sin(offset / 360 * 3.14378 * 2) * self.radius
+        y = math.cos(offset / 360 * 3.14378 * 2) * self.radius
+        return (x, y)
+
+    @lru_cache(maxsize=None)
+    def get_line_points(self, out_chan_deg, in_chan_deg, inverted=False):
+        """
+        The liquidity paths need to bend inwards, this is done using
+        bezier interpolation.
+        """
+        x, y = self.offset_to_pos(out_chan_deg)
+        x1, y1 = self.offset_to_pos(in_chan_deg)
+        a, b = Vector(x, y), Vector(x1, y1)
+        dot = a.normalized().dot(b.normalized())
+        b_mid_x, b_mid_y = lerp_vec(Vector(0, 0), a.mid(b), max(0, dot))
+        import bezier
+
+        curve1 = bezier.Curve(
+            [
+                [x, b_mid_x, x1],
+                [y, b_mid_y, y1],
+            ],
+            degree=2,
+        )
+        points = []
+        step = 100
+        for f in range(step):
+            e = curve1.evaluate(f / step)
+            points.extend([float(e[int(inverted)]), float(e[int(not inverted)])])
+        return points if not inverted else points[::-1]
+
+    @lru_cache(maxsize=None)
     def get_matrix(self, direction: Direction):
         """
         Compute the flow matrix for the channels.
