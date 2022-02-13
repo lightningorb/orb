b4c152527dbc20098857c7259acea418f6cecfb7
orb <admin@lnorb.com>
2022-02-13 15:18:58 +0800

    [dev] upgraded site to include evaluation copy (pre-migration)

---
+++ build_system/alembic.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: lnorb.com
+# @Date:   2022-02-13 13:32:06
+# @Last Modified by:   lnorb.com
+# @Last Modified time: 2022-02-13 14:02:21
+
+from invoke import task
+
+# alembic upgrade head
+# alembic revision -m "eval license" --autogenerate
+# alembic upgrade head
