28fed11758058f50cebe9988cc6bf499c827a869
orb <admin@lnorb.com>
2022-03-04 09:09:26 +0800

    Add ability to hide keyboard in console output. #147

--- orb/screens/console/console_output.py
+++ orb/screens/console/console_output.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: lnorb.com
+# @Date:   2022-01-26 18:25:08
+# @Last Modified by:   lnorb.com
+# @Last Modified time: 2022-03-04 09:06:48
+
 from kivy.properties import StringProperty
 from kivy.uix.textinput import TextInput
 
+from orb.misc.utils import mobile
 from orb.misc import data_manager
 
 
@@ -10,6 +17,8 @@ class ConsoleOutput(TextInput):
     def on_touch_down(self, touch):
 
         if self.collide_point(*touch.pos):
+            if mobile and not touch.is_mouse_scrolling:
+                return False
             if data_manager.data_man.menu_visible:
                 return False
         return super(ConsoleOutput, self).on_touch_down(touch)
