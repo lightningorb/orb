32b4b49b1b15d9706d1597d8c45ae24f0ad6d73e
orb <admin@lnorb.com>
2022-08-29 16:28:12 +0800

    set default number of works workers to 1

--- orb/cli/web.py
+++ orb/cli/web.py
@@ -2,7 +2,7 @@
 # @Author: lnorb.com
 # @Date:   2022-08-29 07:33:02
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-08-29 14:01:14
+# @Last Modified time: 2022-08-29 16:27:57
 
 from orb.web.app import serve as web_serve
 from typing import Optional
@@ -17,7 +17,7 @@ def serve(
     port: int = typer.Option(8080, help="The port to serve."),
     reload: bool = typer.Option(False, help="Live reloading (dev)."),
     debug: bool = typer.Option(False, help="Show debug info (dev)."),
-    workers: int = typer.Option(3, help="Number of web workers."),
+    workers: int = typer.Option(1, help="Number of web workers."),
 ):
     """
     Serve the Orb web app.
