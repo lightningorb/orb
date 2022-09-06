dcc996b2a5dcb9353b834ceb67117d9411b3a9c5
orb <admin@lnorb.com>
2022-09-07 07:43:00 +0800

    orb node delete

--- build_system/casts/orb-node-delete.sh
+++ build_system/casts/orb-node-delete.sh
@@ -1,6 +1,8 @@
 # @Author: lnorb.com
 # @Date:   2022-09-06 15:40:41
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-06 15:40:45
+# @Last Modified time: 2022-09-07 07:41:47
 
 orb node delete
+
+#$ expect deleted
