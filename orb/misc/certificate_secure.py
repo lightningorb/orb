fe4761fcb442d7e2ea718259a87085c13635ca26
orb <admin@lnorb.com>
2022-09-06 13:08:14 +0800

    cli cert handling now hex encoded

--- orb/misc/certificate_secure.py
+++ orb/misc/certificate_secure.py
@@ -2,7 +2,9 @@
 # @Author: lnorb.com
 # @Date:   2022-01-09 08:41:00
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-01-25 13:15:12
+# @Last Modified time: 2022-09-06 12:51:52
+
+import codecs
 
 from orb.misc.certificate import Certificate
 from orb.misc.sec_rsa import *
@@ -20,6 +22,15 @@ class CertificateSecure:
     def init_from_plain(text, uid=None):
         _, pub = get_sec_keys(uid=uid)
         encrypted = encrypt_long(str.encode(text), pub, True)
+        return CertificateSecure(encrypted)
+
+    @staticmethod
+    def init_from_hex(cert_hex, uid=None):
+        if type(cert_hex) is str:
+            cert_hex = cert_hex.encode()
+        text = codecs.decode(cert_hex, "hex")
+        _, pub = get_sec_keys(uid=uid)
+        encrypted = encrypt_long(text, pub, True)
         return CertificateSecure(encrypted)
 
     def as_plain_certificate(self):
--- orb/misc/macaroon_secure.py
+++ orb/misc/macaroon_secure.py
@@ -2,7 +2,7 @@
 # @Author: lnorb.com
 # @Date:   2022-01-09 08:41:00
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-08-22 09:14:20
+# @Last Modified time: 2022-09-06 12:34:37
 
 from orb.misc.macaroon import Macaroon
 from orb.misc.sec_rsa import *
@@ -18,26 +18,20 @@ class MacaroonSecure:
 
     @staticmethod
     def init_from_plain(bin_data, uid=None):
+        """
+        This is super confusing. Is it plain or hex? Clearly hex
+        so ought to be renamed.
+        """
         if type(bin_data) is str:
             bin_data = bin_data.encode()
         _, pub = get_sec_keys(uid=uid)
         encrypted = encrypt_long(bin_data, pub, True)
         return MacaroonSecure(encrypted)
 
-    # @staticmethod
-    # def init_from_not_sure(data):
-    #     if type(data) is str:
-    #         data = data.encode()
-    #     # try decrypting it, if that doesn't work
-    #     try:
-    #         mac = MacaroonSecure.as_plain_macaroon(data)
-    #         return mac
-    #     except:
-    #         pass
-    #     mac = Macaroon.init_from_not_sure(data.decode())
-    #     return MacaroonSecure.init_from_plain(mac.macaroon.encode())
-
     def as_plain_macaroon(self):
+        """
+        Plain or hex?
+        """
         priv, _ = get_sec_keys()
         plain = decrypt_long(self.macaroon_secure, priv)
         return Macaroon.init_from_not_sure(plain)
