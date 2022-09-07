208cbeb4fe6cf3434a726de7b2148d50fd334612
orb <admin@lnorb.com>
2022-09-07 12:38:47 +0800

    cast

--- build_system/casts/orb-peer-list.sh
+++ build_system/casts/orb-peer-list.sh
@@ -1,6 +1,8 @@
 # @Author: lnorb.com
 # @Date:   2022-09-06 15:30:20
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-06 15:30:29
+# @Last Modified time: 2022-09-07 12:38:20
 
 orb peer list
+
+#$ expect 02d41224b71a5346a656f8949c66d11495e39dac55ab8772f55c26ca515db910ea
