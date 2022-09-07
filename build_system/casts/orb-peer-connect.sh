5f6942854d15fcd1615d022881cefceeac76617f
orb <admin@lnorb.com>
2022-09-07 12:52:27 +0800

    cast

--- build_system/casts/orb-peer-connect.sh
+++ build_system/casts/orb-peer-connect.sh
@@ -1,8 +1,10 @@
 # @Author: lnorb.com
 # @Date:   2022-09-06 15:32:28
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-07 12:45:27
+# @Last Modified time: 2022-09-07 12:51:46
 
 #$ wait 2000
 
 orb peer connect $pubkey
+
+#$ expect peer
