096c1de82a7315415ce627a002c157b9a4c24b0d
orb <admin@lnorb.com>
2022-09-08 11:32:40 +0800

    cast

--- build_system/casts/orb-channel-open.sh
+++ build_system/casts/orb-channel-open.sh
@@ -1,10 +1,8 @@
 # @Author: lnorb.com
 # @Date:   2022-09-06 15:28:25
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-07 06:17:18
+# @Last Modified time: 2022-09-08 11:27:31
 
-pk=`orb peer list | head -n 1`
-
-orb channel open $pk 10_000_000 1
+orb channel open $pubkey 10_000_000 1
 
 # expect output_index
