b901c8f6d1a44943088512ac8fe82a3ed35f7959
orb <admin@lnorb.com>
2022-09-09 16:11:48 +0800

    create orb public

--- build_system/casts/orb-node-create-orb-public.sh
+++ build_system/casts/orb-node-create-orb-public.sh
@@ -2,25 +2,7 @@
 # @Author: lnorb.com
 # @Date:   2022-09-06 14:55:53
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-09 15:45:16
+# @Last Modified time: 2022-09-09 16:05:27
 
 orb node create-orb-public cln rest
 #$ expect as default
-orb node info
-#$ expect num_inactive_channels
-orb node delete
-#$ expect deleted
-
-orb node create-orb-public lnd rest
-#$ expect as default
-orb node info
-#$ expect num_inactive_channels
-orb node delete
-#$ expect deleted
-
-orb node create-orb-public lnd grpc
-#$ expect as default
-orb node info
-#$ expect num_inactive_channels
-orb node delete
-#$ expect deleted
