95230f7544fcb91a0b403bf0108856d39cad95df
orb <admin@lnorb.com>
2022-09-13 14:55:47 +0800

    function should not be hidden

--- orb/cli/utils.py
+++ orb/cli/utils.py
@@ -2,17 +2,17 @@
 # @Author: lnorb.com
 # @Date:   2022-08-08 19:08:11
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-07 06:00:01
+# @Last Modified time: 2022-09-13 14:54:27
 
 import os
 from pathlib import Path
 from configparser import ConfigParser
 
-from orb.misc.utils_no_kivy import _get_user_data_dir_static
+from orb.misc.utils_no_kivy import get_user_data_dir_static
 
 
 def get_default_id():
-    conf_path = Path(_get_user_data_dir_static()) / f"orbconnector/orbconnector.ini"
+    conf_path = Path(get_user_data_dir_static()) / f"orbconnector/orbconnector.ini"
     if conf_path.exists():
         conf = ConfigParser()
         conf.read(conf_path.as_posix())
