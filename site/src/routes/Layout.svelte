390bd0905dee7b42e0d6b829d5b632e52c414039
orb <admin@lnorb.com>
2023-02-05 16:06:30 +0800

    added

--- site/src/routes/Layout.svelte
+++ site/src/routes/Layout.svelte
@@ -4,7 +4,6 @@
 	import AboutUs from '../Components/AboutUs.svelte';
 	import Services from '../Components/Services.svelte';
 	import Gallery from '../Components/Gallery.svelte';
-	import Pricing from '../Components/Pricing.svelte';
 	import Footer from '../Components/Footer.svelte';
 	import Download from './../Components/Download.svelte';
 	import Testimonial from './../Components/Testimonial.svelte';
@@ -18,7 +17,6 @@
 <AboutUs />
 <Services serviceData={data.SERVICE_DATA} websiteData={data.WEBSITE_DESCRIPTION} />
 <Gallery galleryData={data.GALLERY_DATA}/>
-<!-- <Pricing pricingData={data.PRICING_DATA} /> -->
 <Download />
 <Testimonial testimonialData={data.TESATIMONIAL_DATA} />
 <Footer />
