7d30fba1b3cd0934df2de4970cea157ce332282a
orb <admin@lnorb.com>
2023-02-08 14:47:14 +0800

    Strip out pyarmor for reproducible builds [issue #5]

--- build_system/armor.py
+++ build_system/armor.py
@@ -2,7 +2,7 @@
 # @Author: lnorb.com
 # @Date:   2022-01-28 05:46:08
 # @Last Modified by:   lnorb.com
-# @Last Modified time: 2022-09-25 10:28:42
+# @Last Modified time: 2023-02-08 13:25:02
 
 try:
     # not all actions install all requirements
@@ -74,24 +74,6 @@ data = [
 ]
 
 
-def upload_to_s3(env, file_name, bucket, object_name=None):
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=env["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=env["AWS_SECRET_ACCESS_KEY"],
-    )
-    try:
-        response = s3_client.upload_file(
-            file_name, bucket, object_name, ExtraArgs={"ACL": "public-read"}
-        )
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-
 def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -99,13 +81,6 @@ def zipdir(path, ziph):
                 os.path.join(root, file),
                 os.path.relpath(os.path.join(root, file), path),
             )
-
-
-@task
-def register(c, env=os.environ):
-    with open("pyarmor-regcode-2364.txt", "w") as f:
-        f.write(env["PYARMOR"])
-    c.run("pyarmor register pyarmor-regcode-2364.txt", env=env)
 
 
 def ubuntu_boostrap_3_8():
@@ -146,7 +121,6 @@ pip install --upgrade --force-reinstall pillow
 
 def build_common(c, env, sep=":"):
     global data
-    register(c)
     spec = ""
     if sep == ";":
         # windows detected
@@ -158,53 +132,30 @@ def build_common(c, env, sep=":"):
             if x.is_dir()
         ]
     )
-
-    data = " ".join(f"--add-data '{s}{sep}{d}'" for s, d in data)
-    print("=" * 50)
-    print("DATA PATHS")
-    print(data)
-    print("=" * 50)
+    data = " ".join(f"--add-data {s}{sep}{d}" for s, d in data)
     hidden_imports = "--hidden-import orb.orb_main --hidden-import orb.core --hidden-import orb.core_ui.hidden_imports --hidden-import kivy --hidden-import plyer.platforms.macosx.uniqueid --hidden-import orb.core_ui.kvs --hidden-import orb.misc --hidden-import jaraco.text --hidden-import kivymd.effects.stiffscroll.StiffScrollEffect  --hidden-import fabric --hidden-import=pkg_resources"  # --hidden-import pandas.plotting._matplotlib
-    pyinstall_flags = f" {paths} {data} {hidden_imports} --onedir --{'windowed' if sep == ':' else 'console'} "
-    expiry = arrow.utcnow().shift(years=1)
-    c.run(
-        f"pyarmor licenses --expired {expiry.format('YYYY-MM-DD')} satoshi_0_paid",
-        env=os.environ,
-    )
-    c.run(
-        f"""pyarmor pack {spec} --with-license licenses/satoshi_0_paid/license.lic --name {name} \
-             -e " {pyinstall_flags}" \
-             -x " --no-cross-protection --exclude build --exclude orb/lnd/grpc_generated" main.py""",
-        env=env,
-    )
+    pyinstall_flags = f" {paths} {data} {hidden_imports} --onedir --{'windowed' if sep == ':' else 'console'} --name lnorb"
+
+    print("=" * 50)
+    print(pyinstall_flags)
+    print("=" * 50)
+
+    c.run(f"pyinstaller {pyinstall_flags} main.py")
 
 
 @task
 def build_linux(c, do_upload=True, env=os.environ):
-    register(c)
     c.run("rm -rf dist tmp;")
     c.run("mkdir -p tmp;")
     c.run("cp -r main.py tmp/;")
     c.run("cp -r third_party tmp/;")
     c.run("cp -r orb tmp/;")
     with c.cd("tmp"):
-        expiry = arrow.utcnow().shift(years=1)
-        c.run(
-            f"pyarmor licenses --expired {expiry.format('YYYY-MM-DD')} satoshi_0_paid",
-            env=os.environ,
-        )
-        c.run(
-            "pyarmor obfuscate --with-license licenses/satoshi_0_paid/license.lic --recursive main.py;",
-            env=env,
-        )
-        c.run("rm -rf orb main.py third_party")
-        c.run("mv dist orb")
         for source, target in data:
-            c.run(f"mkdir -p orb/{target}")
-            c.run(f"cp -r ../{source} orb/{target}")
-        with c.cd("orb"):
-            c.run("python main.py test run-all-tests")
-
+            c.run(f"mkdir -p {target}")
+            c.run(f"cp -r ../{source} {target}")
+        # with c.cd("orb"):
+        # c.run("python main.py test run-all-tests")
         with open("tmp/orb/bootstrap_ubuntu_20_04.sh", "w") as f:
             f.write(ubuntu_boostrap_3_8())
         build_name = (
@@ -212,27 +163,12 @@ def build_linux(c, do_upload=True, env=os.environ):
         )
         print(f"BUILD NAME: {build_name}")
         c.run(f"tar czvf {build_name} orb;")
-        if do_upload:
-            upload_to_s3(
-                env,
-                f"tmp/{build_name}",
-                "lnorb",
-                object_name=f"customer_builds/{build_name}",
-            )
 
 
 @task
 def build_osx(c, do_upload=True, env=os.environ):
     build_common(c=c, env=env, sep=":")
-    file_name = dmg(c=c, env=env)
-    print(file_name)
-    # with c.cd("dist/lnorb"):
-    #     c.run("./lnorb test run-all-tests")
-    if do_upload:
-        print(f"Uploading {file_name} to S3: customer_builds/{file_name}")
-        upload_to_s3(
-            env, file_name, "lnorb", object_name=f"customer_builds/{file_name}"
-        )
+    dmg(c, env=env)
 
 
 @task
@@ -240,18 +176,16 @@ def build_windows(c, env=os.environ):
     build_common(c, env, ";")
     build_name = f"orb-{VERSION}-{os.environ['os-name']}-x86_64.zip"
     zipf = zipfile.ZipFile(build_name, "w", zipfile.ZIP_DEFLATED)
-    # for p in Path(".").glob("*.spec"):
-    #     print(p)
-    #     print(p.open().read())
-    # shutil.copyfile(p.as_posix(), "dist")
     with c.cd("dist/lnorb"):
         c.run("lnorb.exe test run-all-tests")
     zipdir("dist", zipf)
     zipf.close()
-    # upload_to_s3(env, build_name, "lnorb", object_name=f"customer_builds/{build_name}")
 
 
+@task
 def dmg(c, env=os.environ):
+    c.run("ls -l . ")
+    c.run("ls -l dist/ ")
     c.run("rm -f *.dmg ")
     c.run(
         f"""
@@ -261,10 +195,10 @@ def dmg(c, env=os.environ):
           --window-pos 200 120 \
           --window-size 800 400 \
           --icon-size 100 \
-          --icon "lnorb.app" 200 190 \
+          --icon "lnorb" 200 190 \
           --app-drop-link 600 185 \
           "{name}.dmg" \
-          "dist/{name}.app"
+          "dist/{name}"
         """,
         env=env,
     )
